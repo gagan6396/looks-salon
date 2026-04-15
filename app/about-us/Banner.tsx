43d6a3ff375984b6708b3164403d6cb59e5d42f4
Gagan <gagandhyani17@gmail.com>
2026-04-15 16:15:21 +0530

    baner

--- app/about-us/Banner.tsx
+++ app/about-us/Banner.tsx
@@ -32,7 +32,7 @@ export default function ImageSlider() {
               key={i}
               src={src}
               alt={`banner-${i}`}
-              className="w-full flex-shrink-0 object-cover h-[200px] sm:h-[320px] md:h-[420px] lg:h-[490px]"
+              className="w-full flex-shrink-0 object-cover h-[200px] sm:h-[320px] md:h-[420px] lg:h-[500px]"
             />
           ))}
         </div>
